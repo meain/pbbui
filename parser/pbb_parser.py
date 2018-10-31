ec0fa954cd71cb0b59801a1a27d91a8a62d08368
Abin Simon <abinsimon10@gmail.com>
2018-10-31 21:33:38 +0530

    parser: fetch from github and parse

--- parser/pbb_parser.py
+++ parser/pbb_parser.py
@@ -1,32 +1,42 @@
 import re
 import json
+import requests
 
-FILENAME = "pbb.md"
+data = requests.get(
+    "https://raw.githubusercontent.com/dylanaraps/pure-bash-bible/master/README.md"
+).text.split('\n')
 
-data = open(FILENAME).read().split("\n")
+
 heading = None
 subheading = None
 content = []
 parsed = {}
 is_code_block = False
+ignored_headers = ["Table of Contents", "FOREWORD", "AFTERWORD"]
+
 for line in data:
-    if line.startswith('```'):
+    if line.startswith("```"):
         is_code_block = not is_code_block
     if line.startswith("##"):
         if not is_code_block:
             content = []
             subheading = re.sub("## *", "", line)
     elif line.startswith("#"):
+        if re.sub("# *", "", line) in ignored_headers:
+            heading = None
+            continue
         if not is_code_block:
             heading = re.sub("# *", "", line)
-    elif line.startswith('<!--'):
+    elif line.startswith("<!--"):
         continue
-    else:
+    elif heading:
         content.append(line)
 
-    if heading not in parsed:
+    if heading not in parsed and heading is not None:
         parsed[heading] = {}
-    if subheading:
+    if subheading and heading in parsed:
         parsed[heading][subheading] = content
 
-open("../src/pbb.js", "w").write('const data = ' + json.dumps(parsed) + '\nexport default data')
+open("../src/pbb.js", "w").write(
+    "const data = " + json.dumps(parsed) + "\nexport default data"
+)
