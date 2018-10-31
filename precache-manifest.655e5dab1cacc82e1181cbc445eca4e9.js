99f52675709fb585d2581c01aa0ed24be1b84660
Abin Simon <abinsimon10@gmail.com>
2018-10-31 22:10:15 +0530

    Updates

---
+++ precache-manifest.655e5dab1cacc82e1181cbc445eca4e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "beca7a45e79ded66ce1c",
+    "url": "/pbbui/static/css/main.d5dbce8d.chunk.css"
+  },
+  {
+    "revision": "beca7a45e79ded66ce1c",
+    "url": "/pbbui/static/js/main.beca7a45.chunk.js"
+  },
+  {
+    "revision": "968ed3ee8e1d645d2f0a",
+    "url": "/pbbui/static/js/1.968ed3ee.chunk.js"
+  },
+  {
+    "revision": "701e34dc2077fde9e9c6",
+    "url": "/pbbui/static/js/runtime~main.701e34dc.js"
+  },
+  {
+    "revision": "54af9eb359d0b836066736ba031aa8a0",
+    "url": "/pbbui/static/media/magnifier-tool.54af9eb3.svg"
+  },
+  {
+    "revision": "a039c5a9bd7a55df0011d7a0b5a2e8fd",
+    "url": "/pbbui/index.html"
+  }
+];
