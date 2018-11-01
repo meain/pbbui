7c81ab944be798d79cbb730a978a1b78db268eac
Abin Simon <abinsimon10@gmail.com>
2018-11-01 12:06:48 +0530

    Updates

--- precache-manifest.40429cb0770a420bcb785e331ff14693.js
+++ precache-manifest.40429cb0770a420bcb785e331ff14693.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "bfb44cd1d87a7222212a",
+    "revision": "43cdb0726fb05a31ea1d",
     "url": "/pbbui/static/css/main.8fac1a6e.chunk.css"
   },
   {
-    "revision": "bfb44cd1d87a7222212a",
-    "url": "/pbbui/static/js/main.bfb44cd1.chunk.js"
+    "revision": "43cdb0726fb05a31ea1d",
+    "url": "/pbbui/static/js/main.43cdb072.chunk.js"
   },
   {
     "revision": "968ed3ee8e1d645d2f0a",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/pbbui/static/media/magnifier-tool.54af9eb3.svg"
   },
   {
-    "revision": "fcc94c5d394167a621d10757fa33baea",
+    "revision": "2b152be2cfb04fe8da910f5ebe455820",
     "url": "/pbbui/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pbbui/precache-manifest.119363d13126cb0cca31e09e3d796874.js"
+  "/pbbui/precache-manifest.40429cb0770a420bcb785e331ff14693.js"
 );
 
 workbox.clientsClaim();
