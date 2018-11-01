d19e6a0acc01a84be350de2d3a82aab87d6896bb
Abin Simon <abinsimon10@gmail.com>
2018-11-01 12:28:00 +0530

    footer: add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+// @flow
+import * as React from 'react'
+import './Footer.css'
+
+
+const Footer = () =>{
+    return (
+      <div className='Footer'>
+        <span>
+          Content credits: <a target="_blank" href="https://github.com/dylanaraps/pure-bash-bible">dylanaraps/pure-bash-bible</a>
+        </span>
+        <span>
+          Webpage code: <a target="_blank" href="https://github.com/meain/pbbui">meain/pbbui</a>
+        </span>
+      </div>
+    )
+  }
+
+export default  Footer
