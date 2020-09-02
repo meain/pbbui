49586b0fe3c23a63131a3761ca10419211b970fa
Abin Simon <abinsimon10@gmail.com>
2020-09-02 13:13:48 +0530

    Updates

---
+++ precache-manifest.f460bbc0802e2f34ff6c7ec5c8a3dc17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd362f4648a4c69eb9de2d1d5341b675",
+    "url": "/pbbui/index.html"
+  },
+  {
+    "revision": "7c26e16d21393f53cf7c",
+    "url": "/pbbui/static/css/main.89f5085c.chunk.css"
+  },
+  {
+    "revision": "16c9ab77a3e309bef548",
+    "url": "/pbbui/static/js/2.fdb7b0fe.chunk.js"
+  },
+  {
+    "revision": "7c26e16d21393f53cf7c",
+    "url": "/pbbui/static/js/main.c497843b.chunk.js"
+  },
+  {
+    "revision": "628d1780ad8bc854cc20",
+    "url": "/pbbui/static/js/runtime-main.16b02a6f.js"
+  },
+  {
+    "revision": "54af9eb359d0b836066736ba031aa8a0",
+    "url": "/pbbui/static/media/magnifier-tool.54af9eb3.svg"
+  },
+  {
+    "revision": "cfb49f55eb8da55ee5e66a7587b12849",
+    "url": "/pbbui/static/media/undraw_empty_xct9.cfb49f55.svg"
+  }
+]);
