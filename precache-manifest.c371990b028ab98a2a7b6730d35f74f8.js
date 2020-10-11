9cb568d4067ca627328e3c39290ec48bf2cb56db
meain <meain@users.noreply.github.com>
2020-10-11 10:26:11 +0000

    Deploying to gh-pages from  @ 0dc92fb9690d6669b265b7d59e55a83d55899508 🚀

---
+++ precache-manifest.c371990b028ab98a2a7b6730d35f74f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1161ad03957aa5666bd24c4cb3b51092",
+    "url": "/pbbui/index.html"
+  },
+  {
+    "revision": "7bceeb6d7538cd88faba",
+    "url": "/pbbui/static/css/main.1fcc0fa3.chunk.css"
+  },
+  {
+    "revision": "f4cc5c308170202eab2a",
+    "url": "/pbbui/static/js/2.f8b20f2c.chunk.js"
+  },
+  {
+    "revision": "f8188ee829f50f5ad9480c1196b6c699",
+    "url": "/pbbui/static/js/2.f8b20f2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bceeb6d7538cd88faba",
+    "url": "/pbbui/static/js/main.250b5661.chunk.js"
+  },
+  {
+    "revision": "04e341b1e80fdf6a728c",
+    "url": "/pbbui/static/js/runtime-main.87628a85.js"
+  },
+  {
+    "revision": "54af9eb359d0b836066736ba031aa8a0",
+    "url": "/pbbui/static/media/magnifier-tool.54af9eb3.svg"
+  },
+  {
+    "revision": "cfb49f55eb8da55ee5e66a7587b12849",
+    "url": "/pbbui/static/media/undraw_empty_xct9.cfb49f55.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pbbui/precache-manifest.c547c9942838a17121621e5cb1956520.js"
+  "/pbbui/precache-manifest.c371990b028ab98a2a7b6730d35f74f8.js"
 );
 
 self.addEventListener('message', (event) => {
